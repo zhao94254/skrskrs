c4070087582d7cd3b9a273abaa65a9b3421b4896
zhaopengyun <zhao942548838@qq.com>
2019-03-07 09:28:02 +0800

    init pspider

--- pspider.py
+++ pspider.py
@@ -3,3 +3,20 @@
 # @Created on    : 2019-03-06 17:19
 # @Author  : zpy
 # @Software: PyCharm
+
+import abc
+from abc import ABCMeta
+
+
+class Pspider(metaclass=ABCMeta):
+
+    @abc.abstractmethod
+    def task(self):
+        pass
+
+    @abc.abstractmethod
+    def req_resp(self):
+        pass
+
+    def start(self):
+        pass
