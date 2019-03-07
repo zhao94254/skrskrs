c8f33bb2233a024ba2256b87fd267750e214b56f
zhaopengyun <zhao942548838@qq.com>
2019-03-07 15:04:18 +0800

    v1

---
+++ example/testspider.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 2019-03-07 12:28
+# @Author  : zpy
+# @Software: PyCharm
+
+from pspider import Pspider, req
+
+
+
+class SiteSpider(Pspider):
+
+    def task(self):
+        return "https://www.97up.cn"
+
+    def req_resp(self):
+
+        @req()
+        def first_page():
+            url = self.task()
+            return {"request":{
+                    'url': url,
+                },
+                "response":{
+                    "handler": self.parse_data,
+                }}
+        yield first_page
+
+    def parse_data(self, resp):
+        html = resp.content
+
+if __name__ == '__main__':
+    sp = SiteSpider()
+    sp.start()
