ca0229a7fd1b3e524c94296bcdb6477bdefcfcb8
zhaopengyun <zhao942548838@qq.com>
2019-03-07 18:15:34 +0800

    add model

--- model.py
+++ model.py
@@ -6,6 +6,8 @@
 
 # 定义数据结构，对结果进行检查
 
+from exceptions import ModelError
+
 class SDict(dict):
 
     def __getattr__(self, item):
@@ -17,14 +19,39 @@ class SDict(dict):
 
 class BaseModel:
 
-    def __init__(self):
-        pass
+    def __init__(self, mds):
+        """
+        :param mds:[(sp, int), (ps, str)]
+        """
+        self.mds = mds
+        self.mds_map = {}
+        self.res = SDict()
+        self.check()
+
+    def _check_mds(self):
+        for i in self.mds:
+            if len(i) != 2:
+                raise ModelError
+            self.mds_map[i[0]] = i[1]
+            if isinstance('str', i[1]):
+                self.res[i[0]] = "NULL"
+            elif isinstance(1, i[1]):
+                self.res[i[0]] = -1
+            elif isinstance(1.0, i[1]):
+                self.res[i[0]] = -1.0
 
     def check(self):
-        pass
+        self._check_mds()
+
+    def sql(self):
+        return "insert into {table} {keys} values{}"
 
 
 if __name__ == '__main__':
-    s = SDict()
-    s.d = 2
-    print(s)
+    # s = SDict()
+    # s.d = 2
+    # print(s)
+
+    tmodel = BaseModel([('name', str), ('age', int)])
+    tmodel.res.ks = 2
+    print(tmodel.res)
